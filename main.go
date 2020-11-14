7d022f24dd9bd2b323fdc854f68cdc84ba48efc5
Doug Hellmann <dhellmann@redhat.com>
2020-11-14 11:02:40 -0500

    initialize EventSubscription API
    $ kubebuilder create api --group redfish --version v1alpha1 --kind EventSubscription
    Create Resource [y/n]
    y
    Create Controller [y/n]
    y
    Writing scaffold for you to edit...
    apis/redfish/v1alpha1/eventsubscription_types.go
    controllers/redfish/eventsubscription_controller.go
    Running make:
    $ make
    /home/dhellmann/go/bin/controller-gen object:headerFile="hack/boilerplate.go.txt" paths="./..."
    go fmt ./...
    go vet ./...
    go build -o bin/manager main.go
    
    Signed-off-by: Doug Hellmann <dhellmann@redhat.com>

--- main.go
+++ main.go
@@ -25,6 +25,9 @@ import (
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	redfishv1alpha1 "github.com/metal3-io/redfish-event-controller/apis/redfish/v1alpha1"
+	redfishcontroller "github.com/metal3-io/redfish-event-controller/controllers/redfish"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -36,6 +39,7 @@ var (
 func init() {
 	_ = clientgoscheme.AddToScheme(scheme)
 
+	_ = redfishv1alpha1.AddToScheme(scheme)
 	// +kubebuilder:scaffold:scheme
 }
 
@@ -62,6 +66,14 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&redfishcontroller.EventSubscriptionReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("EventSubscription"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "EventSubscription")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
