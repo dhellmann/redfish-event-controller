8e4a82a4227265e23d1f0be168596ace3aeabb71
Doug Hellmann <dhellmann@redhat.com>
2020-11-14 11:25:30 -0500

    start sketching in the API
    Signed-off-by: Doug Hellmann <dhellmann@redhat.com>

--- apis/redfish/v1alpha1/eventsubscription_types.go
+++ apis/redfish/v1alpha1/eventsubscription_types.go
@@ -17,25 +17,63 @@ limitations under the License.
 package v1alpha1
 
 import (
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+// EventType defines the kinds of events that can trigger notifications.
+// +kubebuilder:validation:Enum=Alert;ResourceAdded;ResourceRemoved;ResourceUpdated;StatusChange
+type EventType string
+
+const (
+	// AlertEventType indicates a condition exists which requires attention.
+	AlertEventType EventType = "Alert"
+	// ResourceAddedEventType indicates a resource has been added.
+	ResourceAddedEventType EventType = "ResourceAdded"
+	// ResourceRemovedEventType indicates a resource has been removed.
+	ResourceRemovedEventType EventType = "ResourceRemoved"
+	// ResourceUpdatedEventType indicates a resource has been updated.
+	ResourceUpdatedEventType EventType = "ResourceUpdated"
+	// StatusChangeEventType indicates the status of this resource has changed.
+	StatusChangeEventType EventType = "StatusChange"
+)
+
+// HTTPHeader defines one header value for the HTTP request to the
+// callback webhook.
+type HTTPHeader struct {
+	Name  string `json:"name,required"`
+	Value string `json:"value,required"`
+}
 
 // EventSubscriptionSpec defines the desired state of EventSubscription
 type EventSubscriptionSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// HostRef specifies the host that the subscription is associated
+	// with. Only Name and Namespace are required. The named resource
+	// must be a BareMetalHost.
+	HostRef *corev1.ObjectReference `json:"HostRef,omitempty"`
 
-	// Foo is an example field of EventSubscription. Edit EventSubscription_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// DestinationURL defines the endpoint to which event data is
+	// POSTed
+	DestinationURL string `json:"descriptionURL,required"`
+
+	// EventTypes are the kinds of events that should trigger
+	// notifications to be sent to this subscription.
+	EventTypes []EventType `json:"eventTypes,omitempty"`
+
+	// HTTPHeaders are passed in the POST request to the
+	// DestinationURL.
+	HTTPHeaders []HTTPHeader `json:"httpHeaders,omitempty"`
 }
 
 // EventSubscriptionStatus defines the observed state of EventSubscription
 type EventSubscriptionStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// ErrorMessage holds any error encountered when registering the
+	// subscription
+	ErrorMessage string `json:"errorMessage"`
+
+	// ODataID is taken from the `@odata.id` value of a successful
+	// subscription
+	ODataID string `json:"odataID"`
 }
 
 // +kubebuilder:object:root=true
